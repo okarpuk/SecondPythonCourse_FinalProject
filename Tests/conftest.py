1fa28c707d10ccbd206cffe79aa2486c4403bb5e
okarpuk <oleg.karpuk.1982@gmail.com>
2024-01-08 23:44:24 +0300

    Minor correction

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -13,13 +13,13 @@ def browser(request):
     link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
     browser.get(link)
 
-    if language == "fr":
-        print(f"\nstart test with chosen language - {language}...")
-        language_selector = browser.find_element(By.CSS_SELECTOR, "[value='fr']")
+    if language == "es":
+        print(f"\nTest started with chosen language - {language}")
+        language_selector = browser.find_element(By.CSS_SELECTOR, "[value='es']")
         language_selector.click()
         submit_language_button = browser.find_element(By.CSS_SELECTOR, "#language_selector .btn-default")
         submit_language_button.click()
-    # elif ... добавить код для всех необходимых языков
+    # elif ... добавить аналогичный код для всех необходимых языков
     # elif ...
     else:
         raise pytest.UsageError("--language incorrect")
