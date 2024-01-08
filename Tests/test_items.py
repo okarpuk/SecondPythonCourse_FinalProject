8bbee1a9a0744263380e7212cb421a24ad7a59e8
okarpuk <oleg.karpuk.1982@gmail.com>
2024-01-08 23:52:38 +0300

    Minor correction1

--- Tests/test_items.py
+++ Tests/test_items.py
@@ -4,5 +4,5 @@ from selenium.webdriver.common.by import By
 def test_add_to_basket_button_should_be_displayed(browser):
     add_to_basket_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert add_to_basket_button.is_displayed()
-    print("Element was found")
+    print("Button displayed")
     time.sleep(7)
