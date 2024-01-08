907d678895e5384239a115cca813a6dfbf0beb2b
okarpuk <oleg.karpuk.1982@gmail.com>
2024-01-08 23:51:32 +0300

    Minor correction1

--- Tests/test_items.py
+++ Tests/test_items.py
@@ -1,7 +1,7 @@
 import time
 from selenium.webdriver.common.by import By
 
-def test_add_to_cart_button_should_be_able(browser):
+def test_add_to_basket_button_should_be_displayed(browser):
     add_to_basket_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert add_to_basket_button.is_displayed()
     print("Element was found")
