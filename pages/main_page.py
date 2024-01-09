ffa3d804fa723ca81e0b920bba54c41d5fa8a10f
okarpuk <oleg.karpuk.1982@gmail.com>
2024-01-09 23:09:13 +0300

    Changes

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 class MainPage(BasePage): # класс-наследник класса BasePage
     def go_to_login_page(self): # аргумент self , чтобы иметь доступ к атрибутам и методам класса
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link") # Так как браузер у нас хранится как аргумент класса BasePage, обращаться к нему нужно соответствующим образом с помощью self
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK) # Так как браузер у нас хранится как аргумент класса BasePage, обращаться к нему нужно соответствующим образом с помощью self
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, ".container-fluid ul.navbar-right"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" # символ * указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать
