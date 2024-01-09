9fa0d71e21a0774e74a6790476ecc79b0077e8ef
okarpuk <oleg.karpuk.1982@gmail.com>
2024-01-09 23:47:17 +0300

    Changes

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,18 +9,18 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, "Current URL incorrect"
+        # assert True
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL), "Login email field is not presented"
         assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Login password field is not presented"
         assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Login button is not presented"
-        assert True
+        # assert True
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_EMAIL), "Register email field is not presented"
         assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD), "Register password field is not presented"
         assert self.is_element_present(*LoginPageLocators.CONFIRM_REGISTER_PASSWORD), "Confirm register password field is not presented"
         assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON), "Register button is not presented"
-        assert True
+        # assert True
