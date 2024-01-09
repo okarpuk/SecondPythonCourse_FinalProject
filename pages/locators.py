8aabbd7dadf7025b21ae74c6f0eb382cc0c9fb98
okarpuk <oleg.karpuk.1982@gmail.com>
2024-01-09 23:26:13 +0300

    Changes

--- pages/locators.py
+++ pages/locators.py
@@ -4,3 +4,13 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+class LoginPageLocators():
+    LOGIN_EMAIL = (By.ID, "#id_login-username")
+    LOGIN_PASSWORD = (By.ID, "#id_login-password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, ".login_form .btn")
+    REGISTER_EMAIL = (By.ID, "#id_registration-email")
+    REGISTER_PASSWORD = (By.ID, "#id_registration-password1")
+    CONFIRM_REGISTER_PASSWORD = (By.ID, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, ".register_form .btn")
+
+
