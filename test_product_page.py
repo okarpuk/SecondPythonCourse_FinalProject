1ca24864cf6dd3b6a2dd046c30a493ae7f530eca
okarpuk <oleg.karpuk.1982@gmail.com>
2024-01-10 21:50:29 +0300

    Changes

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
-
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_add_product_to_basket(browser):
    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
    page = ProductPage(browser, link)                       # создаем объект класса MainPage, передаем в конструктор экземпляр драйвера и url адрес
    page.open()                                          # открываем страницу
@@ -12,7 +13,22 @@ def test_guest_can_go_to_login_page(browser):
    page.book_price_and_basket_price_should_be_equal()
    page.book_name_and_book_name_in_basket_should_be_equal()
 
-
-   # login_page = LoginPage(browser, browser.current_url) # создаем переменную с новым объектом LoginPage и обязательно передаем ему
-   #                                                      # тот же самый объект драйвера для работы с браузером, а в качестве url передаем текущий адрес
-   # login_page.should_be_login_page()                    # используем методы LoginPage
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+   page = ProductPage(browser, link)
+   page.open()
+   page.add_to_basket()
+   page.solve_quiz_and_get_code()
+   page.book_price()
+   page.book_price_in_basket()
+   page.book_price_and_basket_price_should_be_equal()
+   page.book_name_and_book_name_in_basket_should_be_equal()
