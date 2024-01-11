8d60e054ad3844c1fabaf070b34f2714399752d6
okarpuk <oleg.karpuk.1982@gmail.com>
2024-01-11 21:14:00 +0300

    Changes

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,10 +4,10 @@ from .locators import MainPageLocators
 
 class MainPage(BasePage): # класс-наследник класса BasePage
     def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK) # символ * указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать
         link.click()
 
 
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" # символ * указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
