29f472d3a98e72795f4cbe189b71a509a75f5e69
okarpuk <oleg.karpuk.1982@gmail.com>
2024-01-12 23:36:17 +0300

    Changes

--- test_main_page.py
+++ test_main_page.py
@@ -27,5 +27,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
    page.go_to_basket_page()
    basket_page = BasketPage(browser, browser.current_url)
    basket_page.should_be_empty_basket()
-   # basket_page.should_not_be_goods_in_basket()
-   # basket_page.empty_basket_text_should_be_correct()
